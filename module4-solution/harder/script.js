b7950c23877488fb63dd3936d806b701a1a5fca3
Vinay <pvvinaykumar@yahoo.com>
2020-07-30 04:11:04 +0530

    Assignment 4 Solution modified indentation

--- module4-solution/harder/script.js
+++ module4-solution/harder/script.js
@@ -43,30 +43,30 @@ WARNING!!! WARNING!!!
 // 'speak' method.
 // See Lecture 50, part 1
 /* fill in parts of the 'for' loop to loop over names array */
-for (var name in names) {
-  var firstLetter = names[name].charAt(0);
+  for (var name in names) {
+    var firstLetter = names[name].charAt(0);
 
-  // STEP 11:
-  // Retrieve the first letter of the current name in the loop.
-  // Use the string object's 'charAt' function. Since we are looking for
-  // names that start with either upper case or lower case 'J'/'j', call
-  // string object's 'toLowerCase' method on the result so we can compare
-  // to lower case character 'j' afterwards.
-  // Look up these methods on Mozilla Developer Network web site if needed.
-  // var firstLetter =
+    // STEP 11:
+    // Retrieve the first letter of the current name in the loop.
+    // Use the string object's 'charAt' function. Since we are looking for
+    // names that start with either upper case or lower case 'J'/'j', call
+    // string object's 'toLowerCase' method on the result so we can compare
+    // to lower case character 'j' afterwards.
+    // Look up these methods on Mozilla Developer Network web site if needed.
+    // var firstLetter =
 
-  // STEP 12:
-  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
-  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
-  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-  // name in the loop.
-  /* fill in condition here */
-  if (firstLetter.toLowerCase() === 'j') {
-    byeSpeaker.speak(names[name]);
-    // byeSpeaker.xxxx
-  } else {
-    helloSpeaker.speak(names[name]);
-    // helloSpeaker.xxxx
+    // STEP 12:
+    // Compare the 'firstLetter' retrieved in STEP 11 to lower case
+    // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
+    // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
+    // name in the loop.
+    /* fill in condition here */
+    if (firstLetter.toLowerCase() === 'j') {
+      byeSpeaker.speak(names[name]);
+      // byeSpeaker.xxxx
+    } else {
+      helloSpeaker.speak(names[name]);
+      // helloSpeaker.xxxx
+    }
   }
-}
 })(window);
